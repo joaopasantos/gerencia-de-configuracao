f8493afcdcddceb9fda4b900c82722cf9c01b2a4
João Albuquerque <joaopasantos@proton.me>
2024-05-27 20:18:30 -0300

    Corrige teste

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from routes import hello
 
 class Tests(unittest.TestCase):
         def test_func(self):
-                self.assertEqual(hello(), "Flask?")
+                self.assertEqual(hello(), "Flask!")
 
 if __name__ == '__main__':
         unittest.main()
