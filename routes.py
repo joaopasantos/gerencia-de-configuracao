0e57938fd6dd9bf679ba4b70e46a30f06c488cab
João Albuquerque <joaopasantos@proton.me>
2024-06-09 10:07:57 -0300

    Implementa nova rota

--- routes.py
+++ routes.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
         return "Flask!"
+
+@app.route("/hello")
+def hello():
+    return "Hello!"
+
+@app.route("/hello/<name>")
+def helloName(name):
+    return f'Hello, {escape(name)}!'
 
 if __name__ == "__main__":
     from waitress import serve
